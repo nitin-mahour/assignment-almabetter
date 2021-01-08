1265a30c92f3c2f6ad9e834704acaeed3a0831ae
nitin-mahour <nitin.mahour6@gmail.com>
2021-01-08 19:59:41 +0530

    ~

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .db_config import *
 
-app = Flask(__name__, static_folder='static')
+application = app = Flask(__name__, static_folder='static')
 
 # initialize db
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
